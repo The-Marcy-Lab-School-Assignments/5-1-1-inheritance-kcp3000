5b18275693634a28066e8ee23c0a12825f9d49c1
Kevin <mrkev63@gmail.com>
2024-07-12 20:36:37 -0400

    finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,13 +1,37 @@
 class Quadrilateral {
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1,
+      this.side2 = side2,
+      this.side3 = side3,
+      this.side4 = side4
 
+  }
+  getPerimeter() {
+    const perimeter = this.side1 + this.side2 + this.side3 + this.side4
+    return perimeter
+  }
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+  constructor(side1, side2) {
+    super(side1, side2, side1, side2)
+  }
+  getArea(side, side2) {
+    side = this.side1
+    side2 = this.side2
+    const area = side * side2
+    return area
+  }
 }
 
-class Square {
-
+class Square extends Rectangle {
+  constructor(side1) {
+    super(side1, side1, side1, side1)
+  }
+  getDiagonal() {
+    const diagonal = super.getArea(this.side1, this.side1) * 2
+    return Math.sqrt(diagonal)
+  }
 }
 
 /* Be creative with this one! */
